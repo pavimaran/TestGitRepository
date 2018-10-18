339942c9d05f06c2dbf77c0a0349256fd88e4d0c
psugumaran <psugumaran@dminc.com>
2018-10-18 13:46:55 -0400

    added New Java file

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+  
+ public static void main(String[] args) throws Exception {
+ 
+ for(int i=0;i<15;i++)
+	    	{
+	    		
+	    	
+	    		System.out.println("Email id count:"+i);
+ }
+ 
+ }
+ }
